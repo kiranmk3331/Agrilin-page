5d80c3beda39d274b3b432fef821f00b9bcccc10
kiranmk3331 <kiranmk3331@gmail.com>
2023-10-31 13:43:30 +0530

    Add welcome text

---
+++ src/components/BodyContainer/MainSectionContainer/WelcomeContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WelcomeContainer = () => {
+  return (
+    <div className="mt-4 flex items-end gap-4">
+      <h1 className="font-medium text-2xl">Hi 👋 Austin,</h1>
+      <p className="text-[#6D7D78] text-lg">here's your site overwiew.</p>
+    </div>
+  );
+};
+
+export default WelcomeContainer;
--- src/components/BodyContainer/MainSectionContainer/index.jsx
+++ src/components/BodyContainer/MainSectionContainer/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ImageWeather from "./ImageWeather";
+import WelcomeContainer from "./WelcomeContainer";
 
 const MainSectionContainer = () => {
   return (
     <div className="m-8 w-full">
       <ImageWeather />
+      <WelcomeContainer />
     </div>
   );
 };
